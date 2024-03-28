6d8db36dac3cdbbee4d26627ce03574d34f72690
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-03-27 21:20:07 -0300

    Create aula16.py

---
+++ aula16.py
@@ -0,0 +1,10 @@
+entrada = input('Voce quer ëntrar" ou "sair"? ')
+
+if entrada == 'entrar': 
+    print('Voce entrou no sistema')
+
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+
+else:
+    print('Voce nao digitou nenhuma das opcoes')
