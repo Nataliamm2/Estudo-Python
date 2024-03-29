d9a4c5eb9f08b2e5cc91384b0dbeef417fd867c3
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-03-29 20:07:28 -0300

    Create Aula17.py

---
+++ Aula17.py
@@ -0,0 +1,13 @@
+condicao = True
+
+
+if condicao:
+    print('Este codigo é o codigo do if')
+
+else:
+    print('este é o else do primeiro IF')
+
+if 10 == 10:
+    print('outro if')
+
+print('Fora do bloco do if')
