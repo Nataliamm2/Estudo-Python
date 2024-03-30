32971a832486db6ea52afaca37932fa2a99c3f9b
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-03-29 22:09:38 -0300

    Aula10.py

---
+++ Aula20.py
@@ -0,0 +1,23 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+# maior = 2 > 1
+# menor = 1 < 2
+# maior_ou_igual = 3 >= 2
+# menor_ou_igual = 1 <= 2
+
+# if primeiro_valor > segundo_valor:
+#     print(
+#         f'{primeiro_valor=} é maior do que' 
+#         f'do que{segundo_valor=}')
+          
+# else: 
+#     print(f'{segundo_valor=} é menor que'
+#           f'do que{primeiro_valor=}')
+    
+if primeiro_valor >= segundo_valor:
+    print(f'{primeiro_valor=}  é maior ou igual a '
+f'{segundo_valor}')
+    
+else: 
+    print(f'{segundo_valor=} é maior {primeiro_valor}' )
