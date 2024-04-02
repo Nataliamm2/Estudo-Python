b067bfb6a2345422c08c540815ab0c2ad121d062
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-04-02 10:11:59 -0300

    dia 01/05

--- Aula23.py
+++
@@ -1,8 +0,0 @@
-# senha = input('Digite senha: ')
-
-# if not senha:
-    
-#     print('Senha nao foi digitada')
-   
-print(not True)
-print(not False)
--- Aula25.py
+++
@@ -1,5 +0,0 @@
-nome = 'natalia'
-preco = 10.22
-variavel = '%s, o preco total foi R$%.2f' % (nome, preco)
-print(variavel)
-print('O Hexadecimal de %d é %04X' % (26,26))
