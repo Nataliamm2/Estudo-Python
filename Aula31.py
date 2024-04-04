7076f5655b341503a363579a6acebe7f7c2992f2
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-04-04 01:09:23 -0300

    Aulas dia 03/04

---
+++ Aula31.py
@@ -0,0 +1,22 @@
+# v1 = 'a'
+# v2 = 'b'
+# print(id(v1))
+# print(id(v2))
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('faca algo')
+else:
+    print('nao faca algo')
+
+# print(passou_no_if, passou_no_if is None)
+# print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+    print('Nao passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
