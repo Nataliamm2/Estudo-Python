a7b7f4ebed07690fd9ff4799093d682714e7cdea
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-04-06 22:14:14 -0300

    Create Aula33.py

---
+++ Aula33.py
@@ -0,0 +1,8 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'natália matos'
+# outra_variavel = f'{string[:7]}HAHA{string[7:]}'
+print(string.zfill(17))
+# print(outra_variavel)
