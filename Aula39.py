6eab8c30958b247c9a3b976d60542f96d9a25090
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-04-07 23:13:58 -0300

    Create Aula39.py

---
+++ Aula39.py
@@ -0,0 +1,12 @@
+nome = 'Natalia Matos Santana'
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[4])
+
+indice = 0
+while indice < len(nome): 
+    print(nome[indice])
+    indice += 1 
+    
