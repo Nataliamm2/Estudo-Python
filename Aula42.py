c6d01bd19453daea3f8f1bdfaa2cc3cf2b922c5e
Natalia Matos <53824159+Nataliamm2@users.noreply.github.com>
2024-04-10 23:29:38 -0300

    Dia 10/04

--- Aula42.py
+++ Aula42.py
@@ -2,4 +2,26 @@ frase = 'O Python é uma linguagem de programacao'\
 'multiparadigma.'\
 'Python foi criado pr Guido van Rossum.'
 
-print(frase.count('a'))
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_que_apareceu_mais_vezes = ' '
+while i < len(frase):
+    letra_atual = frase[i]
+    
+    if letra_atual == ' ':
+        i += 1
+        continue
+    
+    quantas_apareceu_mais_vezes_atual = frase.count(letra_atual)
+    
+    if qtd_apareceu_mais_vezes < quantas_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = quantas_apareceu_mais_vezes_atual
+        letra_que_apareceu_mais_vezes = letra_atual
+         
+    i += 1 
+    
+print(
+    'A letra que apareceu mais vezes foi ' 
+      f'"{letra_que_apareceu_mais_vezes}" que apareceu '
+      f'{qtd_apareceu_mais_vezes} x'
+      )
