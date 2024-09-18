db819eb59377622917931b99ff988554ce0c74b6
Natalia Matos <nataliamatos.santana@hotmail.com>
2024-09-18 00:11:17 -0300

    Aula dia 17/09
    Dados em lista

--- Aula 48.1.py
+++ Aula 48.1.py
@@ -1,4 +1,12 @@
 lista =[10,20,30,40,50]
-lista[2] = 300
-del lista[2]
-print(lista[2])
+#lista[2] = 300
+lista.append(60) 
+lista.append(70)
+lista.pop()
+lista.append(80)
+lista.append(90)
+ultimo_valor = lista.pop(2)
+print(lista, 'Removido', ultimo_valor)
+
+#del lista[2]
+#print(lista[2])
---
+++ Aula 48.2.py
@@ -0,0 +1,24 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+lista = [10,20,30,40]
+lista.append('luiz') # type: ignore
+nome = lista.pop()
+lista.insert(100, 5)
+#lista.clear()
+print(lista[4])
+
